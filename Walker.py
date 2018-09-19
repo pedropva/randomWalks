c63147a7bc4b03870eee5ede66c189656b9a605a
pedropva <pedrovivi8@hotmail.com>
2018-09-18 22:09:58 -0300

    refined some things, random walk still have similarity problems

--- Walker.py
+++ Walker.py
@@ -9,24 +9,27 @@ class Walker:
         self.limitY = limitY
 
     # Draws self shape to a given context
-    def draw(self,img):
-        img[self.y,self.x][0] = self.color[0]
-        img[self.y,self.x][1] = self.color[1]
-        img[self.y,self.x][2] = self.color[2]
-
+    def draw(self,img,old):
+        if(old == 1):
+            img[self.y,self.x][0] = self.color[0]
+            img[self.y,self.x][1] = self.color[1]
+            img[self.y,self.x][2] = self.color[2]
+        else:
+            img[self.y,self.x][0] = 255 - self.color[0] 
+            img[self.y,self.x][1] = 255 - self.color[1]
+            img[self.y,self.x][2] = 255 - self.color[2]
 
 
     def walk(self,img):
-        
-        diffs =[10000] * 8
-
+        similaritys =[0] * 8
         #run on all neightboors of the pixel and get their similarity
         for x in range(0,8): 
-            diffs[x] = self.similarity(self.getNeighboor(img,x))
+            similaritys[x] = self.similarity(self.getNeighboor(img,x))
         
-        print(diffs)
-        my_list = ['0'] * (1+diffs[0]) + ['1'] * (1+diffs[1]) + ['2'] * (1+diffs[2]) + ['3'] * (1+diffs[3]) + ['4'] * (1+diffs[4]) + ['5'] * (1+diffs[5]) + ['6'] * (1+diffs[6]) + ['7'] * (1+diffs[7])
-        choice = random.choice(my_list)
+        print(similaritys)
+        probabilitys = ['0'] * (1+similaritys[0]) + ['1'] * (1+similaritys[1]) + ['2'] * (1+similaritys[2]) + ['3'] * (1+similaritys[3]) + ['4'] * (1+similaritys[4]) + ['5'] * (1+similaritys[5]) + ['6'] * (1+similaritys[6]) + ['7'] * (1+similaritys[7])
+        
+        choice = random.choice(probabilitys)
         x,y = self.getNeighboorPos(int(choice))
         self.setRelativePosition(x,y)
 
@@ -40,7 +43,7 @@ class Walker:
         return self
 
     def similarity(self,pixel):
-        value = 1000//abs((pixel[0] - self.color[0])) + abs((pixel[1] - self.color[1])) + abs((pixel[2] - self.color[2]))
+        value = 100//(abs((pixel[0] - self.color[0])) + abs((pixel[1] - self.color[1])) + abs((pixel[2] - self.color[2])))
         return value
 
     def setRelativePosition(self,x,y):
--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 import numpy as np
 import cv2
 import os,math,copy,random
-import walk
+import Walker
 
 '''['EVENT_FLAG_ALTKEY', 'EVENT_FLAG_CTRLKEY', 'EVENT_FLAG_LBUTTON', 'EVENT_FLAG_MBUTTON', 'EVENT_FLAG_RBUTTON', 'EVENT_FLAG_SHIFTKEY', 'EVENT_LBUTTONDBLCLK', 'EVENT_LBUTTONDOWN', 'EVENT_LBUTTONUP', 'EVENT_MBUTTONDBLCLK', 'EVENT_MBUTTONDOWN', 'EVENT_MBUTTONUP', 'EVENT_MOUSEHWHEEL', 'EVENT_MOUSEMOVE', 'EVENT_MOUSEWHEEL', 'EVENT_RBUTTONDBLCLK', 'EVENT_RBUTTONDOWN', 'EVENT_RBUTTONUP']
 '''
 #image we're walking on
-filename = '2.jpg'
+filename = 'reee.jpg'
 img = cv2.imread(filename,1)
 name, extension = os.path.splitext(filename)
 
@@ -20,28 +20,28 @@ def create_walker(event,x,y,flags,param):
 
     if event == cv2.EVENT_LBUTTONDOWN :
         drawing = True
-        
-        walkers.append(walk.Walker(x,y,[255,0,0],img.shape[0],img.shape[1]))
+        walkers.append(Walker.Walker(x,y,[255,0,0],img.shape[0],img.shape[1]))
     if event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
-            walkers.append(walk.Walker(x,y,[255,0,0],img.shape[0],img.shape[1]))
+            walkers.append(Walker.Walker(x,y,[255,0,0],img.shape[0],img.shape[1]))
     if event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
 
 
 #here we create the window 
-cv2.namedWindow('original image')
-cv2.setMouseCallback('original image',create_walker)
+cv2.namedWindow('new image')
+cv2.setMouseCallback('new image',create_walker)
 
 newImg = copy.copy(img)
 while(1):
-    
     for walker in walkers:
+        walker.draw(newImg,0)
+        #walker.completelyRandomWalk()
         walker.walk(img)
-        walker.draw(newImg)
+        walker.draw(newImg,1)
     
-    cv2.imshow('original image',img)
+    #cv2.imshow('original image',img)
     cv2.imshow('new image',newImg)
     if cv2.waitKey(20) & 0xFF == 27:
         break
